30f4bf15c9559719308ae0b39cebb2a2f169d57e
Sky Chang <sancsky@livemail.tw>
2016-04-29 15:17:42 +0800

    Add Cors

--- WSL/CrudWebApi.Api/App_Start/WebApiConfig.cs
+++ WSL/CrudWebApi.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Crud_WebApi.Api
 {
@@ -10,6 +11,10 @@ namespace Crud_WebApi.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API 設定和服務
+
+            // cors
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
